34a6764a3105968c394b308c147e9ca138834f33
Yoga Aditama <62709596+yogaaditamaa@users.noreply.github.com>
2021-12-21 21:27:43 +0700

    Add files via upload

---
+++ Pertemuan 10/getdatamhs.php
@@ -0,0 +1,15 @@
+<?php
+
+	require_once "koneksi.php";
+
+	$sql 	= "SELECT * FROM mahasiswa";
+	$query 	= mysqli_query($con,$sql);
+	while ($row = mysqli_fetch_assoc($query)) {
+		$data[] = $row;
+	}
+	
+	header('content-type: application/json');
+	echo json_encode($data);
+	mysqli_close($con);
+
+?>
